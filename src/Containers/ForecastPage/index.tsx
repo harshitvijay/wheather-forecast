cde742ad8f1b05dbf830afbad7c3b3f5142e0460
harshitvijay <harshitvijay007@gmail.com>
2021-10-05 01:15:36 +0530

    Fetched 5 Days Weather Data

--- src/Containers/ForecastPage/index.tsx
+++ src/Containers/ForecastPage/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { Box } from "@mui/material";
 import WeatherCard from "../../Components/WeatherCard";
 import Header from "../../Components/Header";
@@ -7,6 +7,21 @@ import { useStyles } from "../../Styles";
 
 const ForecastPage: FC = () => {
   const classes = useStyles();
+  const fetchData = async() => {
+    try {
+      const weatherData = await fetch("https://api.openweathermap.org/data/2.5/forecast?q=Bengaluru&appid=f1ee0c6370a1c9875b0fd546de222926").then((res) => res.json());
+      console.log(weatherData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+    const interval = setInterval(() => {
+      fetchData();
+    }, 300000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <Box>
       <Header />
