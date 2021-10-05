1d046d72b38e6bcdf2def8b635dd03c471926714
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-05 22:35:58 +0530

    Updated Weather Card Component

--- src/Components/WeatherCard/index.tsx
+++ src/Components/WeatherCard/index.tsx
@@ -1,26 +1,37 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 import { useHistory } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
-import { WeatherStateData } from "../../Context";
+import { useWeatherData } from "../../Context";
 import cloud from "../../assest/icons/cloud.png";
-import { useStyles } from "../../Styles";
+import { dateToLocalDay } from "../../constant";
+import { useStyles } from "./style";
 
 const WeatherCard: FC = () => {
-  const context = useContext(WeatherStateData);
+  const data = useWeatherData();
   const history = useHistory();
   const classes = useStyles();
+  console.log(dateToLocalDay("2021-10-05 15:00:00"));
+
   return (
     <Card
       className={classes.weatherCard}
-      onClick={() => history.push(`/${context.city}/?day=${context.day}`)}
+      onClick={() =>
+        history.push(
+          `/${data.weatherData.city.name}/?day=${data.weatherData.city.country}`
+        )
+      }
     >
       <CardContent>
-        <Typography variant="h4" className={classes.dayStyles}>Friday</Typography>
-        <Typography className={classes.dateStyles}>March 1st, 1:00pm</Typography>
-        <Avatar src={cloud} className={classes.avatarStyles}/>
+        <Typography variant="h4" className={classes.dayStyles}>
+          Friday
+        </Typography>
+        <Typography className={classes.dateStyles}>
+          March 1st, 1:00pm
+        </Typography>
+        <Avatar src={cloud} className={classes.avatarStyles} />
         <Typography variant="h4" className={classes.temperatureStyles}>
           35 &#8457;
         </Typography>
