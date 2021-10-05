5a32d877645c05165532910eba15223e846f2371
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-05 22:34:38 +0530

    Updated Location Component

--- src/Components/Location/index.tsx
+++ src/Components/Location/index.tsx
@@ -1,16 +1,17 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 import { Typography } from "@mui/material";
-import { WeatherStateData } from "../../Context";
-import { useStyles } from "../../Styles";
+import { useWeatherData } from "../../Context";
+import { useStyles } from "./style";
 
 const Location: FC = () => {
-  // https://api.openweathermap.org/data/2.5/forecast?q=Bengaluru&appid=f1ee0c6370a1c9875b0fd546de222926
-  const context = useContext(WeatherStateData);
   const classes = useStyles();
+  const data = useWeatherData();
+
   return (
     <Typography variant="body1" component="div" className={classes.text}>
-      {context.city}, {context.country}
+      {data.weatherData.city.name}, {data.weatherData.city.country}
     </Typography>
   );
 };
+
 export default Location;
