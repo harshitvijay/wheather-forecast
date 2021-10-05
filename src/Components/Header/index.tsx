668c81877128813f5bf75c19b612fa606e2949f1
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-05 22:33:16 +0530

    Updated Header Component

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,18 +1,17 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { useStyles } from "../../Styles";
+import { useStyles } from "./style";
 
 const Header: FC = () => {
   const classes = useStyles();
+
   return (
-    <React.Fragment>
-      <Container className={classes.headerContainer}>
-        <Typography variant="h1" component="div">
-          5-Day ForeCast.
-        </Typography>
-      </Container>
-    </React.Fragment>
+    <Container className={classes.headerContainer}>
+      <Typography variant="h1" component="div">
+        5-Day ForeCast.
+      </Typography>
+    </Container>
   );
 };
 
