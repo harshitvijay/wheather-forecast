47f24436d83bb303cd057a1697601b42b8dce7ca
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-05 22:39:40 +0530

    Added Hourly Forecast Page Container

---
+++ src/Containers/HourlyForecastPage/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Container } from "@mui/material";
+
+const HourlyForecastPage: FC = () => {
+  return <Container>Hello World</Container>;
+};
+
+export default HourlyForecastPage;
