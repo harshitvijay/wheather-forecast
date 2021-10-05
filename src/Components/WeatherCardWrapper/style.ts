f0162b73b972a8b7e7d46bb13e5a9a2c403c991c
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-05 22:37:39 +0530

    Added Weather Card Component Style

---
+++ src/Components/WeatherCardWrapper/style.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  weather: {
+    display: "flex",
+    justifyContent: "space-evenly",
+    margin: "auto",
+    marginTop: "-1.3rem",
+    maxWidth: "80%",
+  },
+});
+
+export { useStyles };
