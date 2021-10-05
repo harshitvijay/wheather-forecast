4cebeaedd4b81f9de880694c57499b85525f69f7
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-05 22:33:56 +0530

    Added Header Style

---
+++ src/Components/Header/style.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  headerContainer: {
+    backgroundColor: "rgb(80, 80, 80, 0.3)",
+    display: "flex !important",
+    height: "40vh",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export { useStyles };
