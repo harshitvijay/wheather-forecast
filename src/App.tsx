2ecae3c632b4183e8c9d577e7e4427dfecd6a871
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-06 20:15:34 +0530

    Fixes Routing Issues

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App: FC = () => {
             <ForecastPage />
           </Route>
           <Route
-            path={`${data.weatherData.city.name}/?day=${data.weatherData.city.country}`}
+            path={`/${weatherData.city.name}`}
             component={HourlyForecastPage}
           ></Route>
         </Switch>
