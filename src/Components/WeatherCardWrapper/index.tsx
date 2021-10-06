3089246b6ccc50e2e4e5e3350eda1ee09906b39e
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-06 10:06:57 +0530

    Updated Weather Card Wrapper Component

--- src/Components/WeatherCardWrapper/index.tsx
+++ src/Components/WeatherCardWrapper/index.tsx
@@ -6,11 +6,11 @@ import { useStyles } from "./style";
 
 const WeatherCardWrapper: FC = () => {
   const classes = useStyles();
-  const { setWeatherData } = useWeatherData();
+  const { weatherData, setWeatherData } = useWeatherData();
   const fetchData = async () => {
     try {
       const data = await fetch(
-        "https://api.openweathermap.org/data/2.5/forecast?q=Bengaluru&appid=f1ee0c6370a1c9875b0fd546de222926"
+        "https://api.openweathermap.org/data/2.5/forecast?q=Indore&appid=f1ee0c6370a1c9875b0fd546de222926"
       ).then((res) => res.json());
       setWeatherData(data);
     } catch (error) {
@@ -27,11 +27,9 @@ const WeatherCardWrapper: FC = () => {
 
   return (
     <Box className={classes.weather}>
-      <WeatherCard />
-      <WeatherCard />
-      <WeatherCard />
-      <WeatherCard />
-      <WeatherCard />
+      {(weatherData.list.filter((listData, index) =>
+        weatherData.list.findIndex(obj => obj.dt_txt.split(" ")[0] === listData.dt_txt.split(" ")[0]) === index))
+        .map((data, index) => index < 5 ? <WeatherCard key ={index} obj = {data} city = {weatherData.city}/> : null)}
     </Box>
   );
 };
