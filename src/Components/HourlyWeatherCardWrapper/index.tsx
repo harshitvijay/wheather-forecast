f28a34184d4eb994ef8fff385c85f0fae581cfc1
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-07 20:07:56 +0530

    Added Hourly Weather Card Component Wrapper

---
+++ src/Components/HourlyWeatherCardWrapper/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from "react";
+import Box from "@mui/system/Box";
+import HourlyWeatherCard from "../HourlyWeatherCard";
+import { useWeatherData } from "../../Context";
+import { DataInterface } from "./hourlyWeatherCardWrapper.interface";
+import useStyles from "./style";
+
+const HourlyWeatherCardWrapper: FC = () => {
+  const classes = useStyles();
+  const data = useWeatherData();
+  const date = new URLSearchParams(window.location.search).get("date");
+  console.log(date);
+  console.log(data);
+
+  return (
+    <Box className={classes.cardWrapper}>
+      {data.weatherData.list.map((listItem: DataInterface, index: number) =>
+        date === listItem.dt_txt.split(" ")[0] ? (
+          <HourlyWeatherCard
+            key={index}
+            data={listItem}
+            city={data.weatherData.city}
+          />
+        ) : null
+      )}
+    </Box>
+  );
+};
+
+export default HourlyWeatherCardWrapper;
---
+++ src/Components/HourlyWeatherCardWrapper/style.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  cardWrapper: {
+    display: "flex !important",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    margin: "5%",
+  },
+});
+
+export default useStyles;
