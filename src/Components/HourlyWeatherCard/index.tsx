04f8052eae564d857145b466cceb2ef7bc650ed1
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-07 20:07:10 +0530

    Added Hourly Weather Card Component

--- src/Components/HourlyWeatherCard/index.tsx
+++ src/Components/HourlyWeatherCard/index.tsx
@@ -1,13 +1,34 @@
 import { FC } from "react";
-import { useWeatherData } from "../../Context";
+import Card from "@mui/material/Card";
+import Typography from "@mui/material/Typography";
+import useStyles from "./style";
+import { CityInterface, DataInterface } from "./hourlyWeatherCard.interface";
 
-const HourlyWeatherCard: FC = () => {
-  const data = useWeatherData();
-  const date = new URLSearchParams(window.location.search).get("date");
-  console.log(date);
-  console.log(data);
+const HourlyWeatherCard: FC<{ data: DataInterface; city: CityInterface }> = ({
+  data,
+  city,
+}) => {
+  const classes = useStyles();
+  console.log(data, city);
 
-  return <div></div>;
+  return (
+    <Card className={classes.card}>
+      <Typography>Current Time: {data.dt_txt.split(" ")[1]}</Typography>
+      <Typography>
+        Sunrise: {new Date(city.sunrise * 1000).toTimeString().split(" ")[0]}
+      </Typography>
+      <Typography>
+        Sunset: {new Date(city.sunset * 1000).toTimeString().split(" ")[0]}
+      </Typography>
+      <Typography>
+        Min temp: {Math.floor(data.main.temp_min - 273.15)} &#8451;
+      </Typography>
+      <Typography>
+        Max Temp: {Math.floor(data.main.temp_max - 273.15)} &#8451;
+      </Typography>
+      <Typography>Wind Speed: {data.wind.speed} km/h</Typography>
+    </Card>
+  );
 };
 
 export default HourlyWeatherCard;
