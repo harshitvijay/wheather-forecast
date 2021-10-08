cf9759b6cb6f8f21799f5ee2efc8efb631e81a71
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-08 18:03:35 +0530

    Updated Util  Function File

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,36 @@ export const dateToLocalDay = (date: string): string => {
     new Date(date)
   );
 };
+
+export const tempKelvinToCelcius = (temp: number): number => {
+  return Math.floor(temp - 273.15);
+};
+
+export const timeStampToTimeString = (timeStamp: number): string => {
+  return new Date(timeStamp * 1000).toTimeString().split(" ")[0];
+};
+
+export const dateTimeStringToTimeString = (dateTime: string): string => {
+  return dateTime.split(" ")[1];
+};
+export const dateTimeStringToDateString = (dateTime: string): string => {
+  return dateTime.split(" ")[0];
+};
+
+export const fetchData = async (setWeatherData: Function, url: string) => {
+  try {
+    const data = await fetch(url).then((res) => res.json());
+    setWeatherData(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const fetchCities = async (url: string) => {
+  try {
+    const cities = await fetch(url).then((res) => res.json());
+    return cities.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
