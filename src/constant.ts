9d256cca994723c51c2b213210cb7f9605030cb4
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-08 18:03:08 +0530

    Updated Constant File

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,3 @@
+export const citiesUrl = "https://countriesnow.space/api/v0.1/countries";
 export const url =
   "https://api.openweathermap.org/data/2.5/forecast?q=Pune&appid=f1ee0c6370a1c9875b0fd546de222926";
