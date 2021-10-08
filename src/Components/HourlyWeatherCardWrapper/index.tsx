e589bc8f2f5a48e858e6942f6f9386e9537caca8
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-08 18:05:26 +0530

    Updated Hourly Weather Card Wrapper Component

--- src/Components/HourlyWeatherCardWrapper/index.tsx
+++ src/Components/HourlyWeatherCardWrapper/index.tsx
@@ -1,8 +1,15 @@
 import { FC } from "react";
 import Box from "@mui/system/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
 import HourlyWeatherCard from "../HourlyWeatherCard";
 import { useWeatherData } from "../../Context";
 import { DataInterface } from "./hourlyWeatherCardWrapper.interface";
+import { dateTimeStringToDateString } from "../../utils";
 import useStyles from "./style";
 
 const HourlyWeatherCardWrapper: FC = () => {
@@ -12,15 +19,42 @@ const HourlyWeatherCardWrapper: FC = () => {
 
   return (
     <Box className={classes.cardWrapper}>
-      {data.weatherData.list.map((listItem: DataInterface, index: number) =>
-        date === listItem.dt_txt.split(" ")[0] ? (
-          <HourlyWeatherCard
-            key={index}
-            data={listItem}
-            city={data.weatherData.city}
-          />
-        ) : null
-      )}
+      <TableContainer>
+        <Table className={classes.table}>
+          <TableHead className={classes.tableHeader}>
+            <TableRow>
+              <TableCell className={classes.tableHeaderRowCell}>Time</TableCell>
+              <TableCell className={classes.tableHeaderRowCell}>
+                Sunrise
+              </TableCell>
+              <TableCell className={classes.tableHeaderRowCell}>
+                Sunset
+              </TableCell>
+              <TableCell className={classes.tableHeaderRowCell}>
+                Min Temp (&#8451;)
+              </TableCell>
+              <TableCell className={classes.tableHeaderRowCell}>
+                Max Temp (&#8451;)
+              </TableCell>
+              <TableCell className={classes.tableHeaderRowCell}>
+                Wind speed (km/h)
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.weatherData.list.map(
+              (listItem: DataInterface, index: number) =>
+                date === dateTimeStringToDateString(listItem.dt_txt) ? (
+                  <HourlyWeatherCard
+                    key={index}
+                    data={listItem}
+                    city={data.weatherData.city}
+                  />
+                ) : null
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
--- src/Components/HourlyWeatherCardWrapper/style.ts
+++ src/Components/HourlyWeatherCardWrapper/style.ts
@@ -7,6 +7,18 @@ const useStyles = makeStyles({
     justifyContent: "space-between",
     margin: "5%",
   },
+  table: {
+    minWidth: 500,
+    backgroundColor: "rgb(80, 80, 80, 0.3)",
+  },
+  tableHeader: {
+    backgroundColor: "black",
+  },
+  tableHeaderRowCell: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
 
 export default useStyles;
