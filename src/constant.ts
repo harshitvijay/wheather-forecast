c814c9b396aa6d00f884887dc1edf15f792ea576
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-11 22:37:16 +0530

    Updated Constant File

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,12 @@
 export const citiesUrl = "https://countriesnow.space/api/v0.1/countries";
 export const url =
   "https://api.openweathermap.org/data/2.5/forecast?q=Pune&appid=f1ee0c6370a1c9875b0fd546de222926";
+
+export const staticData = [
+  "Time",
+  "Sunrise",
+  "Sunset",
+  "Min Temp (℃)",
+  "Max Temp (℃)",
+  "Wind Speed (km/h)",
+];
