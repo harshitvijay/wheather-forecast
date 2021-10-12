b9c9e7885e748a47695948e82c30ba35c6215c9e
Harshit Vijayvargiya <harshitvijay007@gmail.com>
2021-10-12 10:08:33 +0530

    Updated Forecast  Page Container

--- src/Containers/ForecastPage/index.tsx
+++ src/Containers/ForecastPage/index.tsx
@@ -1,23 +1,43 @@
 import { FC } from "react";
-import Container from "@mui/material/Container";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "../../Components/Header";
 import Location from "../../Components/Location";
 import WeatherCardWrapper from "../../Components/WeatherCardWrapper";
 import HourlyWeatherCardWrapper from "../../Components/HourlyWeatherCardWrapper";
+import RoutesComponentWrapper from "../../Components/RoutesComponentWrapper";
+import { useWeatherData } from "../../Context";
 
 const ForecastPage: FC = () => {
-  return window.location.href === "http://localhost:3000/" ? (
-    <Container>
-      <Header title="5-Day Forecast." />
-      <Location />
-      <WeatherCardWrapper />
-    </Container>
-  ) : (
-    <Container>
-      <Header title="Hourly Forecast." />
-      <HourlyWeatherCardWrapper />
-    </Container>
+  const { weatherData } = useWeatherData();
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <RoutesComponentWrapper
+            child1={<Header title="5-Day Forecast." />}
+            child2={<Location />}
+            child3={<WeatherCardWrapper />}
+          />
+        </Route>
+        <Route path={`/${weatherData.city.name}`}>
+          <Header title="Hourly Forecast." />
+          <HourlyWeatherCardWrapper />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
 export default ForecastPage;
+
+//  <Container>
+//       <Header title="5-Day Forecast." />
+//       <Location />
+//       <WeatherCardWrapper />
+//     </Container>
+//   ) : (
+//     <Container>
+//       <Header title="Hourly Forecast." />
+//       <HourlyWeatherCardWrapper />
+//     </Container>
